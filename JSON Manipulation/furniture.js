b7ad21265ccd462ba8c8c73690c9cf13dae09e75
Lucky Ardhika <49157897+LuckyArd110@users.noreply.github.com>
2021-10-15 14:29:24 +0700

    Find all the furniture solved

---
+++ JSON Manipulation/furniture.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+let rawdata = fs.readFileSync('data.json');
+const data = JSON.parse(rawdata);
+
+function eDevices(data){
+    const results = data.filter(obj => obj.type == "furniture");
+    for (var i = 0; i < results.length; i++){
+        console.log(results[i].name);
+    }
+}
+
+eDevices(data);
