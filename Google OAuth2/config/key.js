f03573283e049b83ef2c849a6421565cd0b1f30a
Lucky Ardhika <49157897+LuckyArd110@users.noreply.github.com>
2021-10-16 10:47:57 +0700

    Sensitive information about database: solved

---
+++ Google OAuth2/config/key.js
@@ -0,0 +1,14 @@
+// sensitive information
+module.exports = {
+    mongodb:{
+      dbURI: "mongodb+srv://training:PJYoyZW6kFod53Gn@clustertrainee.3u2r4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    },
+    google:{
+      clientID:"296029575441-dlkhveokagg3r93fha824hcv1tvlb2uh.apps.googleusercontent.com",
+      clientSecret:"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"
+    },
+    session:{
+        cookieKey:"thisisacookiekey"
+      }
+  };
+  
